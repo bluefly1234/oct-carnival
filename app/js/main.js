e0b43d0c5669f60eeb7bf5e26f9e3cd9aba84c22
kalechao <kalechao@outlook.com>
2016-08-08 15:11:59 +0800

    基础
    基础

---
+++ app/js/main.js
@@ -0,0 +1,24 @@
+/**
+  Author: Kale Chao | FakeCityMan
+  Blog: http://kalechao87.github.io/
+**/
+// 页面背景色变量
+$color1 = '#64caff'; // 封面第一页
+$color2 = '#4f6c9d'; // 第二页
+$color3 = '#a3d0e3'; // 第三页
+$color4 = '#5d5f77'; // 第四页
+$color5 = ''; // 第五页
+$color6 = '#454e8d'; // 第六页
+$color7 = '#0f0c24'; // 第七页
+$color8 = '#9f7954'; // 第八页
+$color9 = '#422a5c'; // 第九页
+$color10 = '#4ba9d5'; // 第十页
+$color11 = '#3870aa'; // 第十一页
+$color11 = '#ff4750'; // 第十二页
+
+// 设置背景图片
+(function($) {
+    $(document).ready(function() {
+        console.log('Ready');
+    });  //Document ready
+})(jQuery);
